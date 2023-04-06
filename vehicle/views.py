abc8c9ac79cd08073c461599e8f8cd132ffa24e0
ADIB-the-NOOB <mdadib550@gmail.com>
2023-04-07 01:12:23 +0600

    authentication class added to Post Req

--- vehicle/views.py
+++ vehicle/views.py
@@ -22,10 +22,11 @@ class VehicleView(views.APIView):
 
     def post(self, request):
         try:
-            serializer = self.serializer_class(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            serializers = self.serializer_class(data=request.data)
+            if serializers.is_valid():
+                serializers.save(user=request.user)
+                return Response(serializers.data, status=status.HTTP_200_OK)
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        

2f2d6786eb11263a22f5b2426d53c3c4cbe5fc41
ADIB-the-NOOB <mdadib550@gmail.com>
2023-04-08 20:15:25 +0600

    Driving Licese model added

--- vehicle/admin.py
+++ vehicle/admin.py
@@ -4,11 +4,17 @@ from . models import Vehicle
 # Register your models here.
 
 class VehicleAdmin(admin.ModelAdmin):
-    list_display = ('user', 'registration_number', 'vehicle_brand',)
+    list_display = ('user', 'registration_number', 'vehicle_brand', 'vehicle_image_preview')
     search_fields = ()
     list_per_page = 25
 
+    def vehicle_image_preview(self, obj):
+        return obj.vehicle_image_preview()
+    
+    vehicle_image_preview.allow_tags = True
+    vehicle_image_preview.short_description = 'Vehicle Image'
 
 admin.site.register(Vehicle, VehicleAdmin)
 admin.site.register(models.VehicleBrand)
 admin.site.register(models.VehicleModel)
+admin.site.register(models.DrivingLicense)
--- vehicle/models.py
+++ vehicle/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.html import format_html
 
 User = get_user_model()
 
@@ -14,7 +15,19 @@ class Vehicle(models.Model):
     def __str__(self):
         return f"{self.user.full_name} - {self.registration_number}"
     
-    
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old_instance = Vehicle.objects.get(pk=self.pk)
+            if old_instance.vehicle_image and self.vehicle_image != old_instance.vehicle_image:
+                old_instance.vehicle_image.delete(save=False)
+            if old_instance.vehicle_image and self.vehicle_image != old_instance.vehicle_image:
+                old_instance.vehicle_image.delete(save=False)
+        super(Vehicle, self).save(*args, **kwargs)
+
+    def vehicle_image_preview(self):
+        if self.vehicle_image:
+            return format_html('<img src="{}" height="100"/>'.format(self.vehicle_image.url))
+
 class VehicleBrand(models.Model):
     
     VEHICLE_TYPE = (
@@ -39,3 +52,16 @@ class VehicleModel(models.Model):
     def __str__(self):
         return f"{self.brand.brand_name} - {self.model_name}"
         
+
+class DrivingLicense(models.Model):
+    Vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='vehicle_driving_license')
+    license_file = models.FileField(upload_to='driving_license')
+
+    def __str__(self):
+        return f"{self.Vehicle.user.full_name} - {self.Vehicle.registration_number}"
+    
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old_obj = DrivingLicense.objects.get(pk=self.pk)
+            old_obj.license_file.delete()
+        super(DrivingLicense, self).save(*args, **kwargs)

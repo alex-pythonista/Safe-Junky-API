fa45495beeed8fb2d88fc689b8c9bf1a29efb38a
ADIB-the-NOOB <mdadib550@gmail.com>
2023-04-08 16:04:21 +0600

    File Rearranged

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from . import views
+from . import views, blood_group
 from django.urls import path
 
 urlpatterns = [
@@ -10,6 +10,6 @@ urlpatterns = [
 
     # Emergency Information
     path('emergency-info/', views.EmergencyInfoView.as_view(), name='emergency-info'),
-    path('add-blood-group/', views.create_blood_group, name='add-blood-group'),
-    path('update-blood-group/', views.update_blood_group, name='update-blood-group'),
+    path('add-blood-group/', blood_group.create_blood_group, name='add-blood-group'),
+    path('update-blood-group/', blood_group.update_blood_group, name='update-blood-group'),
     ]
--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
+
 
 from . import serializers, models
 from utils.azure_email_service import send_email
@@ -141,40 +141,3 @@ class EmergencyInfoView(generics.GenericAPIView):
             }, status=status.HTTP_400_BAD_REQUEST)
         
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-@authentication_classes([TokenAuthentication])
-def create_blood_group(request):
-    try:
-        data = request.data
-        blood_group = data.get('blood_group')
-        user = models.User.objects.get(id=request.user.id)
-        models.EmergencyInformation.objects.create(user=user, blood_group=blood_group)
-        return Response({
-            'blood_group': blood_group,
-            'message': 'Blood group added successfully'
-        }, status=status.HTTP_200_OK)
-    except Exception as e:
-        return Response({
-            'error': str(e)
-        }, status=status.HTTP_400_BAD_REQUEST)
-    
-        
-@api_view(['PUT'])
-@permission_classes([IsAuthenticated])
-@authentication_classes([TokenAuthentication])
-def update_blood_group(request):
-    try:
-        data = request.data
-        blood_group = data.get('blood_group')
-        emergency_info = models.EmergencyInformation.objects.get(user=request.user)
-        emergency_info.blood_group = blood_group
-        emergency_info.save()
-        return Response({
-            'Updated_blood_group': blood_group,
-            'message': 'Blood group updated successfully'
-        }, status=status.HTTP_200_OK)
-    except Exception as e:
-        return Response({
-            'error': str(e)
-        }, status=status.HTTP_400_BAD_REQUEST)
